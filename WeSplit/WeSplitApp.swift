0ff378e7a7ef30098ae8ea9903b4f4fb95e0ec39
Indra <indracahyadi@icloud.com>
2023-08-13 08:16:19 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Indra Cahyadi on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
